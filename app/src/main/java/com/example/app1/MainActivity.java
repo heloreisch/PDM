0387181e1a3bd62c40a49e2b433b45b3002144a0
heloreisch <heloisa.mafrat@gmail.com>
2026-08-21 11:43:58 -0300

    botao contador

--- app/src/main/java/com/example/app1/MainActivity.java
+++ app/src/main/java/com/example/app1/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.app1;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,12 +11,27 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    int contador =0;
 
-    @Override
+
+
+    //repara a tela básica do sistema, e faz o aplicativo ocupar o espaço inteiro do visor
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
+
         setContentView(R.layout.activity_main);
+        //classe R = referencia / id = identificador unico do botao
+        Button b = findViewById(R.id.button);
+        //classe textView  variavel tv = identificador textview (alterado no layout)
+        TextView tv = findViewById(R.id.textView);
+      // b variavel botao chama a funçao de clica acontece tal coisa
+        // v {} função anonima
+        b.setOnClickListener(view -> {
+            contador++;
+            tv.setText(Integer.toString(contador));
+            // int p string
+        });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
