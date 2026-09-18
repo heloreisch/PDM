1046b3d4a46cbf1dc2630ffae452187db2835284
heloreisch <heloisa.mafrat@gamil.com>
2026-09-18 12:00:37 -0300

    imc

--- app/src/main/java/com/example/app1/MainActivity.java
+++ app/src/main/java/com/example/app1/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.app1;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +13,7 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
 
     @Override
+    int  posicao = 0;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
@@ -20,5 +23,19 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        Integer[] imagens = new Integer[]{
+                R.drawable.perfil,
+                R.drawable.abaixopeso};
+        //colocar todos
+        Button botaoVoltar, botaoProximo;
+        botaoVoltar = findViewById(R.id.btnVoltar);
+        botaoProximo = findViewById(R.id.btnProximo);
+        ImageView imageView = findViewById(R.id.imageView);
+
+        botaoProximo.setOnClickListener( View -> {
+            posicao ++;
+            imageView.setImageResource(Imagens[posicao]);
+        });
+
     }
 }
